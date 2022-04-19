8e06102357ee49c1b1b7132cd6009266411880ed
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-04-19 15:47:05 +0500

    commit

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,2 @@
+import React, { useState } from "react";
+import { useAuth } from "../../context/Authlogin";
---
+++ src/context/Authlogin.jsx
@@ -0,0 +1,36 @@
+import { createContext, useContext, useEffect } from "react";
+import { useState } from "react";
+
+const AuthContext = createContext();
+
+export default AuthContext;
+
+export function AuthProvider({children}){
+  const [token,setToken] = useState(
+    window.localStorage.getItem("token")
+  );
+
+
+  useEffect(()=>{
+    if(token){
+      window.localStorage.setItem("token",token);
+    }
+    else{
+      window.localStorage.removeItem("token")
+    }
+  },[token])
+
+  return(
+    <AuthContext.Provider value={{token,setToken}}>
+      <AuthContext.Consumer>
+        {() => children}
+      </AuthContext.Consumer>
+    </AuthContext.Provider>
+  )
+}
+
+export function useAuth(e){
+  const {token,setToken} = useContext(AuthContext);
+
+  return [token,setToken];
+}
