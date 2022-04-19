0b4af844dea2900488188d85d798ba68cb84311b
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-04-19 15:57:12 +0500

    new file

---
+++ src/components/Svgs/Customer.js
@@ -0,0 +1,7 @@
+const Customer = () =>{
+    return(
+        <svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg"><mask id="a" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="20" height="20"><path fill="#C4C4C4" d="M0 0h20v20H0z"/></mask><g mask="url(#a)"><path d="M5.75 5.75A3.755 3.755 0 0 0 9.5 9.5a3.755 3.755 0 0 0 3.75-3.75A3.755 3.755 0 0 0 9.5 2a3.755 3.755 0 0 0-3.75 3.75Zm10.417 12.083H17V17a5.84 5.84 0 0 0-5.833-5.833H7.833A5.84 5.84 0 0 0 2 17v.833h14.167Z" fill="#fff"/></g></svg>
+    )
+}
+
+export default Customer
