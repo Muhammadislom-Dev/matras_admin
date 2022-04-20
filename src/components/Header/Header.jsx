533943940b75a148b54e2b531935846a96ea5fa8
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-04-20 11:19:34 +0500

    commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,112 +1,62 @@
-import { useState, useEffect } from 'react'
+
 import './Header.scss'
-import Toggle from '../Toggle/Toggle';
-import constants from '../../configs/constants';
+
 
 const Header = ({items}) =>{
-    const [data, setData] = useState([]);
+    // const [data, setData] = useState([]);
 
-    const getOrders = async() =>{
-        let token = window.localStorage.getItem("token")
+    // const getOrders = async() =>{
+    //     let token = window.localStorage.getItem("token")
 
-        let response = await fetch(constants.API_URL+"/v1/orders", {
-            method: "GET",
-            headers:{
-                "Content-Type": "application/json",
-                "Authorization": token
-            }
-        })
-        response = await response.json();
-        if(response?.data.order){
-            setData(response?.data.order)
-        }
-    }
+    //     let response = await fetch(constants.API_URL+"/v1/orders", {
+    //         method: "GET",
+    //         headers:{
+    //             "Content-Type": "application/json",
+    //             "Authorization": token
+    //         }
+    //     })
+    //     response = await response.json();
+    //     if(response?.data.order){
+    //         setData(response?.data.order)
+    //     }
+    // }
 
-    const deleteCustomerService = async(id) => {
-        let token = window.localStorage.getItem("token") 
-		let response = await fetch(constants.API_URL+`/v1/customers/delete/${id}`,{
-			method: "DELETE", 
-			headers: {
-				"Content-Type": "application/json",
-                "Authorization": token
-			}
-		})  
+    //  const deleteCustomerService = async(id) => {
+    //     let token = window.localStorage.getItem("token") 
+	// 	let response = await fetch(constants.API_URL+`/v1/customers/delete/${id}`,{
+	// 		method: "DELETE", 
+	// 		headers: {
+	// 			"Content-Type": "application/json",
+    //             "Authorization": token
+	// 		}
+	// 	})  
 
-        response = await response.json();
-        // console.log(response);
-        if(response.ok == true) {
+    //     response = await response.json();
+    //     // console.log(response);
+    //     if(response.ok == true) {
             
-            getOrders()
-        }
-    } 
+    //         getOrders()
+    //     }
+    // }
 
-    useEffect(async () => {
-       await getOrders()  
-    }, []) 
+    // useEffect(async () => {
+    //    await getOrders()  
+    // }, []) 
 
     return(
-        // <div className="header">
-        //     <div className="header__page">
-        //         <table className='header__list'>
-        //             <th className='header__item'>
-        //                 <td className='header__name'>
-        //                     ID
-        //                 </td>
-        //                 <td className='header__name'>
-        //                     Sana
-        //                 </td>
-        //                 <td className='header__name'>
-        //                     Telefon Raqami
-        //                 </td>
-        //                 <td className='header__name'>
-        //                     Qayta aloqa
-        //                 </td>
-        //             </th>
-        //         </table>
-        //     </div>
-        // </div>
-
-        <section className="tables">
-    <div className="tbl-header">
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Ismi</th>
-                    <th>Telefon Raqami</th>
-                    <th>Mahsulot Nomlari</th>
-                    <th>Miqdor</th>
-                    <th>Qayta Aloqa</th>
-                </tr>
-            </thead>
-        </table>
-    </div>
-    <div className="tbl-content">
-        <table>
-            <tbody>
-                {data.length > 0 && data.map((order,i) =>(
-                <tr className="tr" key={i}>
-                    <td className="td-left">{order.order_id}</td>
-                    <td>{order.user_name}</td>
-                    <td>{order.user_phone}</td>
-                    <td>{order.order_name}</td>
-                    <td>{order.order_amount}</td>
-                    <td className="td-right">
-                        <div className="toggle"> 
-                            <input type="checkbox" id={order.order_contacted} />
-                            <label className="toggle-label" htmlFor={order.order_contacted}>Toggle</label> 
-                        </div>
-                    </td>
-                </tr>
-
-                ))}
-            </tbody>
-        </table>
-
-    </div>
+        <div className="header">
+            
+                            
+                            
+                            
+                            {/* <label class="switch">
+                            <input type="checkbox" />
+                            <span class="slider round"></span>
+                            </label> */}
+        </div>
 
 
-</section>
+
 
     )
 }
