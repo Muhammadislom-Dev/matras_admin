e7fae8ae085d0cda77e8393389a2ae1ec6ccec1f
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-04-20 12:06:37 +0500

    commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,63 +1,90 @@
-
 import './Header.scss'
 
 
-const Header = ({items}) =>{
-    // const [data, setData] = useState([]);
+const Header = () =>{
 
-    // const getOrders = async() =>{
-    //     let token = window.localStorage.getItem("token")
 
-    //     let response = await fetch(constants.API_URL+"/v1/orders", {
-    //         method: "GET",
-    //         headers:{
-    //             "Content-Type": "application/json",
-    //             "Authorization": token
-    //         }
-    //     })
-    //     response = await response.json();
-    //     if(response?.data.order){
-    //         setData(response?.data.order)
-    //     }
-    // }
-
-    //  const deleteCustomerService = async(id) => {
-    //     let token = window.localStorage.getItem("token") 
-	// 	let response = await fetch(constants.API_URL+`/v1/customers/delete/${id}`,{
-	// 		method: "DELETE", 
-	// 		headers: {
-	// 			"Content-Type": "application/json",
-    //             "Authorization": token
-	// 		}
-	// 	})  
-
-    //     response = await response.json();
-    //     // console.log(response);
-    //     if(response.ok == true) {
-            
-    //         getOrders()
-    //     }
-    // }
-
-    // useEffect(async () => {
-    //    await getOrders()  
-    // }, []) 
-
-    return(
-        <div className="header">
-            
-                            
-                            
-                            
-                            {/* <label class="switch">
+return(
+<div className="header">
+    <div>
+        <table className="header__page">
+            <thead>
+                <tr className='header__list'>
+                    <th className='header__name'>ID</th>
+                    <th className='header__name'>Ismi</th>
+                    <th className='header__name'>Telefon raqami</th>
+                    <th className='header__name'>Mahsulot Nomlari</th>
+                    <th className='header__name'>Miqdor</th>
+                    <th className='header__name'>Qayta Aloqa</th>
+                </tr>
+            </thead>
+            <tbody className='header__title'>
+                <tr>
+                    <td className='header__names'>6</td>
+                    <td className='header__names'>Jenny Wilson</td>
+                    <td className='header__names'>+998 90 123 45 67</td>
+                    <td className='header__names'>Ortopedik Eko matras</td>
+                    <td className='header__names'>4</td>
+                    <td className='header__names'>
+                        <label class="switch">
                             <input type="checkbox" />
                             <span class="slider round"></span>
-                            </label> */}
-        </div>
+                        </label>
+                    </td>
+                </tr>
+                <tr>
+                    <td className='header__names'>6</td>
+                    <td className='header__names'>Jenny Wilson</td>
+                    <td className='header__names'>+998 90 123 45 67</td>
+                    <td className='header__names'>Ortopedik Eko matras</td>
+                    <td className='header__names'>4</td>
+                    <td className='header__names'>
+                        <label class="switch">
+                            <input type="checkbox" />
+                            <span class="slider round"></span>
+                        </label>
+                    </td>
+                </tr>
+                <tr>
+                    <td className='header__names'>6</td>
+                    <td className='header__names'>Jenny Wilson</td>
+                    <td className='header__names'>+998 90 123 45 67</td>
+                    <td className='header__names'>Ortopedik Eko matras</td>
+                    <td className='header__names'>4</td>
+                    <td className='header__names'>
+                        <label class="switch">
+                            <input type="checkbox" />
+                            <span class="slider round"></span>
+                        </label>
+                    </td>
+                </tr>
+                <tr>
+                    <td className='header__names'>6</td>
+                    <td className='header__names'>Jenny Wilson</td>
+                    <td className='header__names'>+998 90 123 45 67</td>
+                    <td className='header__names'>Ortopedik Eko matras</td>
+                    <td className='header__names'>4</td>
+                    <td className='header__names'>
+                        <label class="switch">
+                            <input type="checkbox" />
+                            <span class="slider round"></span>
+                        </label>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+
+    {/* <label class="switch">
+        <input type="checkbox" />
+        <span class="slider round"></span>
+    </label> */}
+</div>
 
 
 
 
-    )
+)
 }
 export default Header
