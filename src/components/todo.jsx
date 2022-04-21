e2cacd871f6835d3e31fe6943233c8a1b8275540
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-04-21 20:35:10 +0500

    commit

---
+++ src/components/todo.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Todo = () =>{
+    return(
+        <>
+          <div className="main-div">
+              <div className="child-div">
+                  <figure>
+                      
+                  </figure>
+              </div>
+          </div>
+        </>
+    )
+}
+export default Todo
