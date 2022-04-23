ccad390469c40475292f1c2e3bf12a6ff388d286
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-04-23 22:21:22 +0500

    Initial commit

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,36 +1,62 @@
 import './Section.scss'
 
 import deletes from '../../assets/img/delete.png'
+import { useState } from 'react'
 
 const Section =() =>{
+
+  const  [modal, setModal] = useState(false);
+
+  const openModal = (e) =>{
+    e.preventDefault()
+    setModal(!modal)
+  }
+
+  const todo = (event) =>{
+    event.preventDefault()
+
+    setModal(!modal)
+     
+    event.target[0].value = null;
+    event.target[1].value = null;
+
+    console.log(event.target[0].value);
+  }
+
   return(
     <div className="section">
-      <div className="section__page">
-         <table className='section__table'>
+      <div className="section__pages">
+         <table className='section__page'>
            <thead>
              <tr className='section__list'>
                <th className='section__name'>Toifalar</th>
              </tr>
            </thead>
-           <tbody className='section__tbody'>
-              <tr className='section__title'>
-                <td className='section__text'>Model C</td>
-                <td>
+           <tbody className='section__title'>
+              <tr>
+                <td className='section__names'>Model C</td>
+                <td className='section__btns'>
                   <button className='section__btn'>
-                    <img src={deletes} alt="" />
+                    <img className='section__img' src={deletes} alt="" />
                   </button>
                 </td>
               </tr>
               <tr className='section__title'>
-                <td className='section__text'>Model C</td>
-                <td>
+                <td className='section__names'>Model C</td>
+                <td className='section__btns'>
                   <button className='section__btn'>
-                    <img src={deletes} alt="" />
+                    <img className='section__img' src={deletes} alt="" />
                   </button>
                 </td>
               </tr>
            </tbody>
          </table>
+      </div>
+
+      <div className="section__button">
+         <button className="section__buttons">
+            Qo'shish
+         </button>
       </div>
     </div>
   )
